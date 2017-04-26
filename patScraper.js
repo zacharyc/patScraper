6c546c282dcf2ffac6eaf02b1ace4d42eacdeff9
Zachary Cohen <zachary@salesforceiq.com>
2017-04-26 13:17:41 -0700

    Parse the dom out of the html document

--- patScraper.js
+++ patScraper.js
@@ -1,9 +1,19 @@
 // Using tinyrq from this website: https://www.codementor.io/johnnyb/how-to-write-a-web-scraper-in-nodejs-du108266t
 // Simple script for scraping the patagonia website and producing a table of thier stores
 // Will be used on patagoniafanboy.com
-const request = require("tinyreq");
 
-console.log("This should be happening")
-request("http://patagonia.com/", function (err, body) {
-    console.log(err || body); // Print out the HTML
-});
+var fs = require("fs");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+var file = fs.readFileSync("./page.html", "utf8");
+
+const dom = new JSDOM(file);
+
+console.log("here");
+// console.log(file);
+
+var doc = dom.window.document;
+
+var results = doc.querySelector(".stores-header").textContent;
+console.log(results);
